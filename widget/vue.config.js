fa54a2ee6a3306f159f653db7ed46a94a1c2d9f1
dbalas <dbalasdev@gmail.com>
2020-11-20 15:01:05 +0100

    vue widget initial code

---
+++ widget/vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.output.filename = 'js/bannery.js';
+      delete config.output.chunkFilename;
+    }
+  },
+  chainWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.delete('splitChunks');
+    }
+  },
+  css: {
+    extract: false,
+  },
+};
